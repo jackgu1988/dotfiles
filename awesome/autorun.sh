7c79dd9cc0b2e67386a1724678cf4b2b650c4052
Iakovos Gurulian <jack.gurulian@gmail.com>
2017-06-13 12:48:30 +0100

    Added awesomewm config files

---
+++ awesome/autorun.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function run {
+	if ! pgrep "$1" ;
+	then
+		$@&
+	fi
+}
+
+run nm-applet --sm-disable
+run cryptkeeper
+run dropbox start -i
+run ./.dropbox-dist/dropbox-lnx.x86_64-26.4.24/dropbox
+
+#setxkbmap -layout "us,el"
+setxkbmap -layout "us,el" -option "grp:alt_shift_toggle"
+
+# Enable tap to click and natural scroll
+xinput set-prop "SynPS/2 Synaptics TouchPad" "274" 1
+xinput set-prop "SynPS/2 Synaptics TouchPad" "284" 1
+# NOTE: on different HW:
+# check available mice: xinput list
+# check available properties: xinput list-props "MOUSE NAME"
