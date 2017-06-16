676d2716c581b849bb6f5f915e896e435780ef6a
Iakovos Gurulian <jack.gurulian@gmail.com>
2017-06-16 18:03:25 +0100

    Updated rc.lua to better display Spotify notifications (thanks alepmaros for the gist)

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -674,3 +674,19 @@ awful.util.spawn_with_shell("~/.config/awesome/autorun.sh")
 
 -- Start the screensaver
 awful.util.spawn_with_shell("xscreensaver -no-splash")
+
+
+-- Changing spotify notifications.
+naughty.config.presets.spotify = { 
+    -- if you want to disable Spotify notifications completely, return false
+    callback = function(args)
+        return true
+    end,
+
+    -- Adjust the size of the notification
+    height = 100,
+    width  = 400,
+    -- Guessing the value, find a way to fit it to the proper size later
+    icon_size = 90
+}
+table.insert(naughty.dbus.config.mapping, {{appname = "Spotify"}, naughty.config.presets.spotify})
